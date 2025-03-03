399036218181230f4ac7e133109aad658f4a2b2e
SAURAVBORAH22 <sauravborah1999@gmail.com>
2025-03-03 18:11:24 +0530

    changes

--- src/app/profile/userProfile/user-profile-edit/user-profile-edit.component.ts
+++ src/app/profile/userProfile/user-profile-edit/user-profile-edit.component.ts
@@ -16,53 +16,30 @@ export class UserProfileEditComponent implements OnInit {
 
   ngOnInit(): void {
     this.profileForm = new FormGroup({
-      firstName: new FormControl('',
-        [
-          Validators.required
-        ]
-      ),
+      firstName: new FormControl('',[Validators.required]),
       middleName: new FormControl(''),
-      lastName: new FormControl('',
-        [
-          Validators.required
-        ]
-      ),
-      email: new FormControl('',
-        [
-          Validators.required, Validators.email
-        ]
-      ),
-      dateOfBirth: new FormControl('',
-        [
-          Validators.required
-        ]
-      ),
-      addressLine1: new FormControl('',
-        [
-          Validators.required
-        ]
-      ),
+      lastName: new FormControl('',[Validators.required]),
+      email: new FormControl('',[Validators.required, Validators.email]),
+      dateOfBirth: new FormControl('',[Validators.required]),
+      addressLine1: new FormControl('',[Validators.required]),
       addressLine2: new FormControl(''),
-      city: new FormControl('',
-        [
-          Validators.required
-        ]
-      ),
-      state: new FormControl('',
-        [
-          Validators.required
-        ]
-      ),
-      country: new FormControl('',
-        [
-          Validators.required
-        ]
-      ),
-      zipcode: new FormControl('',
-        [
-          Validators.required
-        ]
-      )
+      city: new FormControl('',[Validators.required]),
+      state: new FormControl('',[Validators.required]),
+      country: new FormControl('',[Validators.required]),
+      zipcode: new FormControl('',[Validators.required]),
+      businessName: new FormControl('',[Validators.required]),
+      buisnessType: new FormControl('', [Validators.required]),
+      industry: new FormControl('', [Validators.required]),
+      domain: new FormControl(''),
+      registrationNumber: new FormControl(''),
+      businessEmail: new FormControl('', [Validators.required, Validators.email]),
+      businessPhoneNumber: new FormControl('', [Validators.required]),
+      businessAddressLine1: new FormControl('',[Validators.required]),
+      businessAddressLine2: new FormControl(''),
+      businessCity: new FormControl('',[Validators.required]),
+      businessState: new FormControl('',[Validators.required]),
+      businessCountry: new FormControl('',[Validators.required]),
+      businessZipcode: new FormControl('',[Validators.required]),
     })
   }
 
