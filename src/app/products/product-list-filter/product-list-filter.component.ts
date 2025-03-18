331de0d7762c557bca65460e6e99b3c7f99df20f
SAURAVBORAH22 <sauravborah1999@gmail.com>
2025-03-18 16:01:57 +0530

    filter changes done

--- src/app/products/product-list-filter/product-list-filter.component.ts
+++ src/app/products/product-list-filter/product-list-filter.component.ts
@@ -8,14 +8,31 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class ProductListFilterComponent {
   @Output() filterChanged = new EventEmitter<any>();
 
-  filters: any = { name: '', rating: null, minPrice: null, maxPrice: null };
+  filters: any = {
+    name: '',
+    rating: null,
+    minPrice: 0,
+    maxPrice: 100000
+  };
 
   applyFilters(): void {
-    this.filterChanged.emit({ ...this.filters }); // Emit a copy of the filters
+    this.validatePriceRange();
+    this.filterChanged.emit({ ...this.filters });
   }
 
   resetFilters(): void {
-    this.filters = { name: '', rating: null, minPrice: null, maxPrice: null };
-    this.filterChanged.emit(this.filters); // Emit empty filters
+    this.filters = {
+      name: '',
+      rating: null,
+      minPrice: 0,
+      maxPrice: 100000
+    };
+    this.filterChanged.emit({ ...this.filters });
+  }
+
+  validatePriceRange(): void {
+    if (this.filters.minPrice > this.filters.maxPrice) {
+      [this.filters.minPrice, this.filters.maxPrice] = [this.filters.maxPrice, this.filters.minPrice];
+    }
   }
 }
